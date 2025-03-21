c35af4adddf47a1b03d6a828d38df262f2a2adfe
tony <tmtony98@gmail.com>
2025-03-21 10:39:42 +0530

    first commit

---
+++ app/components/ImageViewer.tsx
@@ -0,0 +1,25 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { StyleSheet } from 'react-native';
+import { Image, type ImageSource } from 'expo-image';
+
+
+type Props = {
+    imgSource: ImageSource;
+    selectedImage : string | undefined; 
+  };
+  
+export default function ImageViewer({ imgSource, selectedImage }: Props) {
+    const imageSource = selectedImage ? { uri: selectedImage } : imgSource;
+  return (
+    <Image source={selectedImage} style={styles.image} />
+  )
+}
+
+const styles = StyleSheet.create({
+    image: {
+      width: 320,
+      height: 440,
+      borderRadius: 18,
+    },
+  })
